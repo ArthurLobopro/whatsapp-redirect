c91f075389574c10bcac638848b6f52d8d9db9cc
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-03-05 18:52:08 -0300

    fix: update to manifest v3

--- src/background.ts
+++ src/background.ts
@@ -1,25 +1,20 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        const url = details.url
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+    const url = new URL(details.url)
 
-        if (url.includes("https://web.whatsapp.com/send?")) {
-            const urlParams = new URLSearchParams(new URL(url).search)
-            const phoneNumber = urlParams.get('phone')
-            const textMessage = urlParams.get('text')!
+    if (url.href.includes("send?")) {
+        const urlParams = new URLSearchParams(new URL(url).search)
+        const phoneNumber = urlParams.get('phone')
+        const textMessage = urlParams.get('text')!
 
-            let newUrl = `whatsapp://send?text=${encodeURIComponent(textMessage)}&phone=${phoneNumber}`
+        let newUrl = `whatsapp://send?text=${encodeURIComponent(textMessage)}&phone=${phoneNumber}`
 
-            openNew(newUrl)
+        openNew(newUrl)
 
-            return { redirectUrl: newUrl }
-        }
+        chrome.tabs.update(details.tabId, { url: newUrl })
+    }
 
-        // Caso a URL não seja a que queremos, não faz nada
-        return {}
-    },
-    { urls: ["https://web.whatsapp.com/*"] },
-    ["blocking"]
-)
+}, { url: [{ hostContains: "web.whatsapp.com" }] })
+
 
 function openNew(url: string) {
     setTimeout(() => chrome.tabs.create({ url }), 2000)
